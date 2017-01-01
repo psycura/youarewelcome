e10489504394a3018cc10b7f1a87f35e256acb68
psycura <psycura@gmail.com>
2017-01-01 12:10:42 +0200

    cleaned

--- web/chat.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $("#addClass").click(function () {
-        $('#sidebar_secondary').addClass('popup-box-on');
-    });
-    
-    $("#removeClass").click(function () {
-        $('#sidebar_secondary').removeClass('popup-box-on');
-    });
-})
